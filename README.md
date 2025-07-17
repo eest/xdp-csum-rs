eb05e83fe9266bbce5652c54e3110ccbf386e18e
Patrik Lundin <patrik@sigterm.se>
2025-07-17 02:12:30 +0200

    Add initial code calling bpf_csum_diff

--- README.md
+++ README.md
@@ -1,5 +1,99 @@
 # xdp-csum-rs
 
+Some WIP code to try out calls to the `bpf_csum_diff` helper for IPv4 and ICMP
+packets.
+
+Currently the program has a hardcoded ICMP length of 64 bytes. This happens to work with the default `ping` on macOS, e.g.:
+```shell
+ping -c 1 xxx.xxx.xxx.xxx
+```
+
+Which makes the program print something like this:
+```shell
+[INFO  xdp_csum_rs] ip_hdr_len: 20
+[INFO  xdp_csum_rs] icmp_offset: 34
+[INFO  xdp_csum_rs] ip_total_len: 84
+[INFO  xdp_csum_rs] original IPv4 checksum: 0xfad6
+[INFO  xdp_csum_rs] recalculated IPv4 checkum: 0xfad6
+[INFO  xdp_csum_rs] IP checksum is valid
+[INFO  xdp_csum_rs] icmp_len: 64
+[INFO  xdp_csum_rs] original ICMP checksum: 0xeffa
+[INFO  xdp_csum_rs] ICMP recalculated checksum: 0xeffa
+[INFO  xdp_csum_rs] ICMP checksum is valid
+```
+
+You can try pinging with some other size and this will just be ignored:
+```shell
+ping -s 67 -c 1 xxx.xxx.xxx.xxx
+```
+
+... resulting in:
+```shell
+[INFO  xdp_csum_rs] ip_hdr_len: 20
+[INFO  xdp_csum_rs] icmp_offset: 34
+[INFO  xdp_csum_rs] ip_total_len: 95
+[INFO  xdp_csum_rs] original IPv4 checksum: 0x1485
+[INFO  xdp_csum_rs] recalculated IPv4 checkum: 0x1485
+[INFO  xdp_csum_rs] IP checksum is valid
+[INFO  xdp_csum_rs] icmp_len: 75
+[INFO  xdp_csum_rs] icmp_len (75) is larger than expected ICMP len (64)
+```
+
+In order to test other lengths (e.g. to check the code handling lengths not a multiple of 4), modify `xdp-csum-rs-ebpf/src/main.rs` and set
+```rust
+const EXPECTED_ICMP_LEN: usize = 64;
+```
+... to whatever is printed in `icmp_len`, so in this case you would set it to
+`75`, which will then handle that specific length (which also happens to
+trigger the code that deals with `bpf_csum_diff` expecting lengths
+being a multiple of 4:
+```
+[INFO  xdp_csum_rs] ip_hdr_len: 20
+[INFO  xdp_csum_rs] icmp_offset: 34
+[INFO  xdp_csum_rs] ip_total_len: 95
+[INFO  xdp_csum_rs] original IPv4 checksum: 0x60bd
+[INFO  xdp_csum_rs] recalculated IPv4 checkum: 0x60bd
+[INFO  xdp_csum_rs] IP checksum is valid
+[INFO  xdp_csum_rs] icmp_len: 75
+[INFO  xdp_csum_rs] original ICMP checksum: 0x6d10
+[INFO  xdp_csum_rs] icmp_len is not a multiple of 4, remainder 3
+[INFO  xdp_csum_rs] initial remainder buf: [0,0,0,0]
+[INFO  xdp_csum_rs] iterating over remainder at 0, offset: 72
+[INFO  xdp_csum_rs] icmphdr_remainder_byte: 40
+[INFO  xdp_csum_rs] iterating over remainder at 1, offset: 73
+[INFO  xdp_csum_rs] icmphdr_remainder_byte: 41
+[INFO  xdp_csum_rs] iterating over remainder at 2, offset: 74
+[INFO  xdp_csum_rs] icmphdr_remainder_byte: 42
+[INFO  xdp_csum_rs] filled in remainder buf: [64,65,66,0]
+[INFO  xdp_csum_rs] ICMP recalculated checksum: 0x6d10
+[INFO  xdp_csum_rs] ICMP checksum is valid
+```
+
+Something else worth noting here is that at some point (I saw it at an
+`icmp_len` above `515`) `bpf_csum_diff` will start returning `-22`
+(`-EINVAL`) so more work is needed to handle larger packets. But at this point
+the main thing to fix would be to make the verifier happy with a dynamically
+calculated payload length so it does not need to hardcoded as it is now.
+
+For reference, triggering the `-EINVAL` can be done by setting `const
+EXPECTED_ICMP_LEN: usize = 516;` and doing
+```shell
+ping -s 508 -c 1 xxx.xxx.xxx.xxx
+```
+
+... resulting in:
+```
+[INFO  xdp_csum_rs] ip_hdr_len: 20
+[INFO  xdp_csum_rs] icmp_offset: 34
+[INFO  xdp_csum_rs] ip_total_len: 536
+[INFO  xdp_csum_rs] original IPv4 checksum: 0x8462
+[INFO  xdp_csum_rs] recalculated IPv4 checkum: 0x8462
+[INFO  xdp_csum_rs] IP checksum is valid
+[INFO  xdp_csum_rs] icmp_len: 516
+[INFO  xdp_csum_rs] original ICMP checksum: 0x7cf0
+[INFO  xdp_csum_rs] invalid call to bpf_csum_diff for ICMP packet: -22
+```
+
 ## Prerequisites
 
 1. stable rust toolchains: `rustup toolchain install stable`
@@ -14,7 +108,7 @@
 Use `cargo build`, `cargo check`, etc. as normal. Run your program with:
 
 ```shell
-cargo run --release --config 'target."cfg(all())".runner="sudo -E"'
+RUST_LOG=info cargo run --config 'target."cfg(all())".runner="sudo -E"' -- --iface eth0
 ```
 
 Cargo build scripts are used to automatically build the eBPF correctly and include it in the
--- xdp-csum-rs-ebpf/src/main.rs
+++ xdp-csum-rs-ebpf/src/main.rs
@@ -1,8 +1,28 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_ebpf::{
+    bindings::xdp_action, helpers::r#gen::bpf_csum_diff, macros::xdp, programs::XdpContext,
+};
 use aya_log_ebpf::info;
+
+use core::mem;
+
+use network_types::{
+    eth::{EthHdr, EtherType},
+    icmp::IcmpHdr,
+    ip::{IpProto, Ipv4Hdr},
+};
+
+// FIXME: Not sure how to make eBPF verifier happy with variable icmp len
+// FIXME: It seems going over an EXPECTED_ICMP_LEN of 515 bytes will make bpf_csum_diff
+// return -22 (-EINVAL), we probably need to further chunk up the calls in this
+// case, maybe around 512 bytes or something.
+const EXPECTED_ICMP_LEN: usize = 64;
+
+// bpf_csum_diff expects length to be a multiple of 4
+const REMAINDER_BUF_SIZE: usize = 4;
+const IPV4_MAXLEN: usize = 60;
 
 #[xdp]
 pub fn xdp_csum_rs(ctx: XdpContext) -> u32 {
@@ -12,9 +32,240 @@ pub fn xdp_csum_rs(ctx: XdpContext) -> u32 {
     }
 }
 
-fn try_xdp_csum_rs(ctx: XdpContext) -> Result<u32, u32> {
-    info!(&ctx, "received a packet");
+// https://aya-rs.dev/book/start/parsing-packets/#__codelineno-1-42
+fn try_xdp_csum_rs(ctx: XdpContext) -> Result<u32, ()> {
+    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
+
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {
+            // mut needed so we can set the exisitng checksum to 0 prior to running bpf_csum_diff
+            let ipv4hdr_mut: *mut Ipv4Hdr = ptr_at_mut(&ctx, EthHdr::LEN)?;
+
+            match unsafe { (*ipv4hdr_mut).proto } {
+                IpProto::Icmp => {
+                    let ip_hdr_len = (unsafe { (*ipv4hdr_mut).ihl() } as usize) * 4;
+                    info!(&ctx, "ip_hdr_len: {}", ip_hdr_len);
+
+                    let icmp_offset = EthHdr::LEN + ip_hdr_len;
+                    info!(&ctx, "icmp_offset: {}", icmp_offset);
+
+                    // Calculate ICMP payload length
+                    let ip_total_len =
+                        unsafe { u16::from_be_bytes((*ipv4hdr_mut).tot_len) } as usize;
+
+                    info!(&ctx, "ip_total_len: {}", ip_total_len);
+
+                    // https://github.com/torvalds/linux/blob/155a3c003e555a7300d156a5252c004c392ec6b0/tools/testing/selftests/bpf/progs/xdp_synproxy_kern.c#L793-L797
+                    if ipv4hdr_mut as usize + IPV4_MAXLEN > ctx.data_end() {
+                        return Err(());
+                    }
+
+                    let orig_ipv4_csum = unsafe { (*ipv4hdr_mut).check };
+                    info!(
+                        &ctx,
+                        "original IPv4 checksum: 0x{:x}{:x}", orig_ipv4_csum[0], orig_ipv4_csum[1]
+                    );
+
+                    // Clear checksum field before calculating new checksum
+                    unsafe { (*ipv4hdr_mut).check = [0, 0] };
+
+                    let recalc_ipv4_csum = unsafe {
+                        bpf_csum_diff(
+                            core::ptr::null_mut(),
+                            0,
+                            ipv4hdr_mut as *mut u32,
+                            ((*ipv4hdr_mut).ihl() * 4) as u32,
+                            0,
+                        )
+                    };
+
+                    // Reset the original check field so the packet is valid again
+                    unsafe { (*ipv4hdr_mut).check = orig_ipv4_csum };
+
+                    if recalc_ipv4_csum < 0 {
+                        info!(&ctx, "invalid recalc_ipv4_csum {}", recalc_ipv4_csum);
+                        return Ok(xdp_action::XDP_PASS);
+                    }
+
+                    let folded_ipv4_csum = csum_fold_helper(recalc_ipv4_csum as u64);
+
+                    info!(
+                        &ctx,
+                        "recalculated IPv4 checkum: 0x{:x}",
+                        u16::to_be(folded_ipv4_csum)
+                    );
+
+                    if u16::to_be(folded_ipv4_csum)
+                        != unsafe { u16::from_be_bytes((*ipv4hdr_mut).check) }
+                    {
+                        info!(&ctx, "IP checksum is invalid");
+                    } else {
+                        info!(&ctx, "IP checksum is valid");
+                    }
+
+                    let icmphdr_mut: *mut IcmpHdr = ptr_at_mut(&ctx, EthHdr::LEN + ip_hdr_len)?;
+
+                    let icmp_len = ctx.data_end() - icmphdr_mut as usize;
+                    info!(&ctx, "icmp_len: {}", icmp_len);
+
+                    if icmp_len > EXPECTED_ICMP_LEN {
+                        info!(
+                            &ctx,
+                            "icmp_len ({}) is larger than expected ICMP len ({})",
+                            icmp_len,
+                            EXPECTED_ICMP_LEN
+                        );
+                        return Ok(xdp_action::XDP_PASS);
+                    }
+
+                    if icmphdr_mut as usize + EXPECTED_ICMP_LEN > ctx.data_end() {
+                        info!(&ctx, "expected ICMP len is larger than size of packet data");
+                        return Ok(xdp_action::XDP_PASS);
+                    }
+
+                    info!(&ctx, "original ICMP checksum: 0x{:x}", unsafe {
+                        u16::from_be((*icmphdr_mut).checksum)
+                    });
+
+                    // Handle uneven data lengths since bpf_csum_diff() expects the length to
+                    // be a multiple of 4
+                    let icmp_len_remainder = icmp_len % REMAINDER_BUF_SIZE;
+                    if icmp_len_remainder != 0 {
+                        info!(
+                            &ctx,
+                            "icmp_len is not a multiple of 4, remainder {}", icmp_len_remainder
+                        );
+                    }
+
+                    let icmp_remainder_offset = icmp_len - icmp_len_remainder;
+
+                    let orig_icmp_checksum = unsafe { (*icmphdr_mut).checksum };
+
+                    // As for IPv4 checksum also reset it for ICMP prior to recalculating it
+                    unsafe { (*icmphdr_mut).checksum = 0 };
+
+                    let mut icmp_csum = unsafe {
+                        bpf_csum_diff(
+                            core::ptr::null_mut(),
+                            0,
+                            icmphdr_mut as *mut u32,
+                            icmp_remainder_offset as u32,
+                            0,
+                        )
+                    };
+
+                    // If there was a remainder we need to add on the final 1-3 bytes padded with
+                    // zeroes using the first call as the seed.
+                    if icmp_len_remainder != 0 {
+                        let mut remainder_buf: [u8; REMAINDER_BUF_SIZE] = [0; REMAINDER_BUF_SIZE];
+                        info!(
+                            &ctx,
+                            "initial remainder buf: [{},{},{},{}]",
+                            remainder_buf[0],
+                            remainder_buf[1],
+                            remainder_buf[2],
+                            remainder_buf[3]
+                        );
+                        for i in 0..icmp_len_remainder {
+                            let offset = icmp_remainder_offset + i;
+                            info!(
+                                &ctx,
+                                "iterating over remainder at {}, offset: {}", i, offset
+                            );
+                            let icmphdr_remainder_byte: *const u8 =
+                                ptr_at(&ctx, EthHdr::LEN + ip_hdr_len + offset)?;
+                            info!(&ctx, "icmphdr_remainder_byte: {:x}", unsafe {
+                                *icmphdr_remainder_byte
+                            });
+                            remainder_buf[i] = unsafe { *icmphdr_remainder_byte };
+                        }
+                        info!(
+                            &ctx,
+                            "filled in remainder buf: [{},{},{},{}]",
+                            remainder_buf[0],
+                            remainder_buf[1],
+                            remainder_buf[2],
+                            remainder_buf[3]
+                        );
+                        icmp_csum = unsafe {
+                            bpf_csum_diff(
+                                core::ptr::null_mut(),
+                                0,
+                                remainder_buf.as_ptr() as *mut u32,
+                                REMAINDER_BUF_SIZE as u32,
+                                icmp_csum as u32,
+                            )
+                        };
+                    }
+
+                    // Add back the checksum now that we are done with verifications.
+                    unsafe { (*icmphdr_mut).checksum = orig_icmp_checksum };
+
+                    if icmp_csum < 0 {
+                        info!(&ctx, "invalid call to bpf_csum_diff for ICMP packet: {}", icmp_csum);
+                        return Ok(xdp_action::XDP_PASS);
+                    }
+
+                    let folded_icmp_csum = csum_fold_helper(icmp_csum as u64);
+
+                    info!(
+                        &ctx,
+                        "ICMP recalculated checksum: 0x{:x}",
+                        u16::to_be(folded_icmp_csum)
+                    );
+
+                    if u16::to_be(folded_icmp_csum)
+                        != unsafe { u16::from_be((*icmphdr_mut).checksum) }
+                    {
+                        info!(&ctx, "ICMP checksum is invalid");
+                    } else {
+                        info!(&ctx, "ICMP checksum is valid");
+                    }
+                }
+                _ => return Ok(xdp_action::XDP_PASS),
+            }
+        }
+        _ => {}
+    }
     Ok(xdp_action::XDP_PASS)
+}
+
+// https://aya-rs.dev/book/start/parsing-packets/#__codelineno-1-29
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+// mut needed for modifiying packet (e.g. resetting checksum field prior to running bpf_csum_diff)
+#[inline(always)]
+fn ptr_at_mut<T>(ctx: &XdpContext, offset: usize) -> Result<*mut T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+    Ok((start + offset) as *mut T)
+}
+
+// https://github.com/riyaolin/aya-ebpf-lb-rs/blob/55bc95f25fc2dc2680cf8add874ad32dcdd3bedd/lb/lb-ebpf/src/main.rs#L60
+#[inline(always)]
+pub fn csum_fold_helper(mut csum: u64) -> u16 {
+    for _i in 0..4 {
+        if (csum >> 16) > 0 {
+            csum = (csum & 0xffff) + (csum >> 16);
+        }
+    }
+    return !(csum as u16);
 }
 
 #[cfg(not(test))]
